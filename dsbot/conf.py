ab279ea504f69039f3b0d2fc07683ff0ac48c5c2
Paul Traylor <kungfudiscomonkey@gmail.com>
2022-07-24 08:30:16 +0900

    Add default to our ignore_users decorator

--- dsbot/conf.py
+++ dsbot/conf.py
@@ -1,6 +1,7 @@
 from django.conf import settings as django_settings
 
 DEFAULTS = {
+    "SLACK_IGNORE_USERS": [],
     # Since we typically deploy 2 workers and we don't want to go too
     # much over the 1 message / second rate limit, we set our rate limit
     # to 0.5 to be equivilant to 1 message / 2 seconds
