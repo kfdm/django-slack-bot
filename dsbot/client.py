0cbba49affee8058838f9b855995dd9996aaf95d
Paul Traylor <kungfudiscomonkey@gmail.com>
2023-11-15 15:49:15 +0900

    Replace RTM Bot with SocketModeAdapter
    RTM Client is somewhat depreciated, and to continue to use Slack bots we
    want to have a new client that mimics what the RTMClient gave us but
    supporting the new socket mode.
    
    We create a new SocketAdapter that provides us a lot of the API previously
    provided by the RTMClient and we simplify our BotClient to use that new
    adapter.
    
    We change our command dispatch to filter on a new 'command' queue so
    that we can reuse more of our dispatch code instead of having a similar
    duplicate like our old RTMClient.
    
    Since a few values change (like data -> messages) we also update some of
    our default plugins/decorators to work with the new structure.

--- dsbot/client.py
+++ dsbot/client.py
@@ -5,120 +5,68 @@ The base bot class is mostly concerned with maintaining
 the connection to Slack, and then dispatching events to
 the Dispatcher
 
-A few convenencie functions used by commands are also
+A few convenience functions used by commands are also
 added to the bot class
 """
-
-
-import concurrent
 import inspect
 import logging
 import re
+from functools import wraps
 
-import slack_sdk.rtm
-import slack_sdk.web
+from asgiref.sync import async_to_sync
+
+from dsbot import decorators
+from dsbot.adapter import SocketAdapter
 
 try:
     from importlib_metadata import entry_points
 except ImportError:
     from importlib.metadata import entry_points
 
-from .exceptions import CommandError
 
 logger = logging.getLogger(__name__)
 
 
-class BotClient(slack_sdk.rtm.RTMClient):
-    user_id = None
-    _commands = []
+class BotClient(SocketAdapter):
+    EVENT_COMMAND = "command"
 
     @classmethod
     def cmd(cls, key):
-        """A decorator to store and link a callback to an event."""
+        """
+        A decorator to store and link a callback to an event.
+
+        Commands are wrapped so that they only match on the provided regex and
+        then they are put onto the listener for command events.
+
+        We also set attributes for key and help to be used by the `help` command
+        """
+        regex = re.compile(key)
 
         def decorator(callback):
-            logger.debug("Registering %s %s", key, callback)
-            cls._validate_callback(callback)
-            cls._commands.append(
-                {
-                    "key": key,
-                    "re": re.compile(key),
-                    "func": callback,
-                    "help": callback.__doc__.strip().split("\n")[0],
-                }
-            )
-            return callback
+            help = callback.__doc__.strip().split("\n")[0]
+            if inspect.iscoroutinefunction(callback):
+                callback = async_to_sync(callback)
+            callback = decorators.command(callback)
+
+            @wraps(callback)
+            def command_wrapper(*args, command, **kwargs):
+                if match := regex.match(command):
+                    return callback(*args, match=match, **kwargs)
+
+            command_wrapper.key = key
+            command_wrapper.help = help
+            cls.register(cls.EVENT_COMMAND, command_wrapper)
+            return command_wrapper
 
         return decorator
 
-    async def _dispatch_command(self, command, data=None):
-        for cmd in self._commands:
-            match = cmd["re"].match(command)
-            if match:
-                try:
-                    if inspect.iscoroutinefunction(cmd["func"]):
-                        logger.debug("Running %(key)s %(func)s as async", cmd)
-                        return await cmd["func"](
-                            rtm_client=self,
-                            web_client=self._web_client,
-                            data=data,
-                            match=match,
-                        )
-                    else:
-                        logger.debug("Running %(key)s %(func)s as thread", cmd)
-                        self._cmd_in_thread(cmd["func"], data=data, match=match)
-                except CommandError as e:
-                    logger.warning("Command Error")
-                    return self._web_client.chat_postEphemeral(
-                        as_user=True,
-                        channel=data["channel"],
-                        user=data["user"],
-                        attachments=[
-                            {
-                                "color": "warning",
-                                "title": "Command Error",
-                                "text": str(e),
-                            }
-                        ],
-                    )
-                except Exception as e:
-                    logger.exception("Unknown Error")
-                    return self._web_client.chat_postEphemeral(
-                        as_user=True,
-                        channel=data["channel"],
-                        user=data["user"],
-                        attachments=[
-                            {
-                                "color": "danger",
-                                "title": "Unknown Error",
-                                "text": str(e),
-                            }
-                        ],
-                    )
-
-    def _cmd_in_thread(self, callback, **kwargs):
-        """Execute the callback in another thread. Wait for and return the results."""
-        web_client = slack_sdk.web.WebClient(
-            token=self.token,
-            base_url=self.base_url,
-            ssl=self.ssl,
-            proxy=self.proxy,
-            headers=self.headers,
-        )
-        with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
-            future = executor.submit(callback, rtm_client=self, web_client=web_client, **kwargs)
-
-            while future.running():
-                pass
-
-            future.result()
-
     @classmethod
     def load_plugins(cls, group="dsbot.commands"):
+        logger.info("Loading plugins for %s", group)
         for entry in entry_points(group=group):
             try:
                 entry.load()
             except ImportError:
                 logger.exception("Error loading %s", entry)
             else:
-                logger.info("Loaded %s", entry)
+                logger.debug("Loaded %s", entry)
--- dsbot/management/commands/bot.py
+++ dsbot/management/commands/bot.py
@@ -1,16 +1,18 @@
 import logging
+import os
 
 from django.core.management.base import BaseCommand
 from django.test import override_settings
+from slack_sdk.web import WebClient
 
 from dsbot.client import BotClient
-from dsbot.conf import settings
 
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
         slack = parser.add_argument_group("Slack arguments")
-        slack.add_argument("--token", default=settings.SLACK_TOKEN, help="Slack token")
+        slack.add_argument("--app-token", default=os.environ.get("SLACK_APP_TOKEN"))
+        slack.add_argument("--bot-token", default=os.environ.get("SLACK_BOT_TOKEN"))
         slack.add_argument("--timeout", default=30, type=int)
         slack.add_argument("--ping-interval", default=30, type=int)
 
@@ -37,7 +39,10 @@ class Command(BaseCommand):
 
         with override_settings(CELERY_TASK_ALWAYS_EAGER=eager):
             BotClient(
-                token=options["token"],
-                timeout=options["timeout"],
+                app_token=options["app_token"],
                 ping_interval=options["ping_interval"],
+                web_client=WebClient(
+                    token=options["bot_token"],
+                    timeout=options["timeout"],
+                ),
             ).start()
